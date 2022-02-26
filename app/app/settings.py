b2f4455d8d8a004f4ad99c6b5809b25c440746df
PrashantRaj18198 <prashant@argonaut.dev>
2022-02-26 12:38:22 +0530

    add 0.0.0.0 as allowed hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '$d5c5h&vkazayd$nevzv2%xrexgv9jv0rauzx(+ue^-0)##&ly'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+]
 
 
 # Application definition
